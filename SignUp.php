3cd9f85925d5ddb959f53b48761f541c57bc1667
GIHAN PRAMOD <gihanpramod86@gmail.com>
2025-01-13 15:35:56 +0530

    connection

--- SignUp.php
+++ SignUp.php
@@ -3,4 +3,17 @@
     $password = $_POST["password"];
     $email = $_POST["email"];
     $phone = $_POST["phone"];
+
+    $conn = new mysqli('localhost','new','','rentitall');
+    if($conn->connect_error) {
+        die('Connection Failed : '.$conn->connect_error);
+    }else{
+        $stmt = $conn->prepare("Instert into account(Username, Password, email, Phone number)
+        values(?,?,?,?) ");
+        $stmt->bind_param("sssi", $username, $password, $email, $phone);  
+        $stmt->execute();
+        echo"Registration Sucessful....";
+        $stmt->close();
+        $conn->close();
+    }
 ?>
