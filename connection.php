ffad5df61bd1754f46be212bbc9aacba374664ca
Pramod Damunupola <pramodashen14612@gmail.com>
2025-01-13 17:29:31 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 // Database configuration
-$host = "DA PMA SignOn:3306";       // Hostname (e.g., localhost or IP address)
+$host = "198.45.114.194";       // Hostname (e.g., localhost or IP address)
 $username = "rentital_root";        // Database username
 $password = "rentitall@123456";            // Database password
 $database = "rentital_rentitall"; // Database name
