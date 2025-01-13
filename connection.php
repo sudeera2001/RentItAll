d7174a107a90a0b7d58c8f0cef5ce94ab17d59ba
Pramod Damunupola <pramodashen14612@gmail.com>
2025-01-13 17:14:12 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
-$host = "127.0.0.1";       // Hostname (e.g., localhost or IP address)
+$host = "DA PMA SignOn";       // Hostname (e.g., localhost or IP address)
 $username = "root";        // Database username
 $password = "";            // Database password
-$database = "rentitall"; // Database name
+$database = "rentitall@123456"; // Database name
 
 // Create a connection
 $conn = new mysqli($host, $username, $password, $database);
